7b85ff6e572f0f4ba58c7b1c57f7a60f76340a3f
atiq-xs <game99cu@gmail.com>
2024-06-24 13:42:56 +0600

    inprogress

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -17,69 +17,56 @@ const showingNavigationDropdown = ref(false);
             <div class="flex">
                 <!-- Left Side Navigation Bar -->
                 <nav
-                    class="fixed top-0 left-0 bg-[#c12f30] p-[21px] border-b border-gray-100 dark:border-gray-700 h-[100vh] w-[256px]"
-                >
+                    class="fixed top-0 left-0 bg-[#c12f30] p-[21px] border-b border-gray-100 dark:border-gray-700 h-[100vh] w-[256px] ">
                     <!-- Vertical Navigation Menu -->
-                    <div class="w-full">
-                        <div>
-                            <div class="">
+                    <div class="w-full h-full ">
+                        <div class="">
+                            <div>
                                 <!-- Logo -->
                                 <div class=" w-[80%] mb-5">
                                     <Link :href="route('dashboard')">
-                                        <ApplicationLogo class="bg-white" />
+                                    <ApplicationLogo class="bg-white" />
                                     </Link>
                                 </div>
 
                                 <!-- Navigation Links -->
                                 <div class="hidden sm:flex ">
-                                    <NavLink
-                                        :href="route('dashboard')"
-                                        :active="route().current('dashboard')"
-                                    >
+                                    <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
                                         <i class=" pi pi-objects-column"></i>
                                         Dashboard
                                     </NavLink>
-                            
+
                                 </div>
+                            </div>
+                            <div class="bg-[#E06F77] p-3 absolute bottom-0 left-0 right-0 rounded">
+                                <div class="relative">
+                                    <span
+                                        class="absolute top-[-21px] bg-[#C12F30] p-2 rounded-full text-2xl font-bold w-[fit-content] mx-auto right-0 left-0 border border-white border-3 text-white"><i
+                                            class="pi pi-question-circle"></i></span>
+                                </div>
+
                             </div>
 
                             <!-- Hamburger -->
                             <div class="-me-2 flex items-center sm:hidden">
-                                <button
-                                    @click="
+                                <button @click="
                                         showingNavigationDropdown =
-                                            !showingNavigationDropdown
-                                    "
-                                    class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out"
-                                >
-                                    <svg
-                                        class="h-6 w-6"
-                                        stroke="currentColor"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                    >
-                                        <path
-                                            :class="{
-                                                hidden: showingNavigationDropdown,
-                                                'inline-flex':
-                                                    !showingNavigationDropdown,
-                                            }"
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            stroke-width="2"
-                                            d="M4 6h16M4 12h16M4 18h16"
-                                        />
-                                        <path
-                                            :class="{
-                                                hidden: !showingNavigationDropdown,
-                                                'inline-flex':
-                                                    showingNavigationDropdown,
-                                            }"
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            stroke-width="2"
-                                            d="M6 18L18 6M6 6l12 12"
-                                        />
+                                        !showingNavigationDropdown
+                                        "
+                                    class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
+                                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                                        <path :class="{
+                                        hidden: showingNavigationDropdown,
+                                        'inline-flex':
+                                            !showingNavigationDropdown,
+                                    }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M4 6h16M4 12h16M4 18h16" />
+                                        <path :class="{
+                                        hidden: !showingNavigationDropdown,
+                                        'inline-flex':
+                                            showingNavigationDropdown,
+                                    }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M6 18L18 6M6 6l12 12" />
                                     </svg>
                                 </button>
                             </div>
@@ -87,30 +74,20 @@ const showingNavigationDropdown = ref(false);
                     </div>
 
                     <!-- Responsive Navigation Menu -->
-                    <div
-                        :class="{
-                            block: showingNavigationDropdown,
-                            hidden: !showingNavigationDropdown,
-                        }"
-                        class="sm:hidden"
-                    >
+                    <div :class="{
+                                        block: showingNavigationDropdown,
+                                        hidden: !showingNavigationDropdown,
+                                    }" class="sm:hidden">
                         <div class="pt-2 pb-3 space-y-1">
-                            <ResponsiveNavLink
-                                :href="route('dashboard')"
-                                :active="route().current('dashboard')"
-                            >
+                            <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
                                 Dashboard
                             </ResponsiveNavLink>
                         </div>
 
                         <!-- Responsive Settings Options -->
-                        <div
-                            class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600"
-                        >
+                        <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
                             <div class="px-4">
-                                <div
-                                    class="font-medium text-base text-gray-800 dark:text-gray-200"
-                                >
+                                <div class="font-medium text-base text-gray-800 dark:text-gray-200">
                                     {{ $page.props.auth.user.name }}
                                 </div>
                                 <div class="font-medium text-sm text-gray-500">
@@ -119,16 +96,10 @@ const showingNavigationDropdown = ref(false);
                             </div>
 
                             <div class="mt-3 space-y-1">
-                                <ResponsiveNavLink
-                                    :href="route('profile.edit')"
-                                >
+                                <ResponsiveNavLink :href="route('profile.edit')">
                                     Profile
                                 </ResponsiveNavLink>
-                                <ResponsiveNavLink
-                                    :href="route('logout')"
-                                    method="post"
-                                    as="button"
-                                >
+                                <ResponsiveNavLink :href="route('logout')" method="post" as="button">
                                     Log Out
                                 </ResponsiveNavLink>
                             </div>
@@ -138,13 +109,9 @@ const showingNavigationDropdown = ref(false);
 
                 <div class="w-full bg-[#f9f9fb] ps-[256px]">
                     <!-- Top Header -->
-                    <div
-                        class="bg-white shadow w-full h-[fit-content] py-5 text-black"
-                    >
+                    <div class="bg-white shadow w-full h-[fit-content] py-5 text-black">
                         <header>
-                            <div
-                                class="hidden sm:flex sm:items-center px-5 justify-between items-center"
-                            >
+                            <div class="hidden sm:flex sm:items-center px-5 justify-between items-center">
                                 <!-- Top header left side Content -->
                                 <div>
                                     <h3 class="text-2xl font-bold">
@@ -158,17 +125,12 @@ const showingNavigationDropdown = ref(false);
                                 <div class="flex items-center gap-3">
                                     <!-- Notification Button -->
                                     <div>
-                                        <button
-                                            class="bg-white shadow p-3 rounded relative z-0"
-                                        >
+                                        <button class="bg-white shadow p-3 rounded relative z-0">
                                             <div class="relative w-6">
-                                                <img
-                                                    src="../../images/notify-icon.png"
-                                                    alt=""
-                                                />
+                                                <img src="../../images/notify-icon.png" alt="" />
                                                 <div
-                                                    class="absolute top-0 right-0 w-3 h-3 border border-white border-[3px] bg-[#db3336] rounded-full z-50"
-                                                ></div>
+                                                    class="absolute top-0 right-0 w-3 h-3 border border-white border-[3px] bg-[#db3336] rounded-full z-50">
+                                                </div>
                                             </div>
                                         </button>
                                     </div>
@@ -176,72 +138,47 @@ const showingNavigationDropdown = ref(false);
                                     <div class="relative">
                                         <Dropdown align="right" width="48">
                                             <template #trigger>
-                                                <span
-                                                    class="inline-flex rounded-md"
-                                                >
-                                                    <button
-                                                        type="button"
-                                                        class="flex items-center gap-3 px-3 py-2 border border-transparent text-sm font-medium rounded-md text-gray-500 dark:text-gray-400 focus:outline-none transition ease-in-out duration-150"
-                                                    >
+                                                <span class="inline-flex rounded-md">
+                                                    <button type="button"
+                                                        class="flex items-center gap-3 px-3 py-2 border border-transparent text-sm font-medium rounded-md text-gray-500 dark:text-gray-400 focus:outline-none transition ease-in-out duration-150">
                                                         <span>
-                                                            <img
-                                                                class="w-12"
-                                                                src="../../images/profile.webp"
-                                                                alt=""
-                                                            />
+                                                            <img class="w-12" src="../../images/profile.webp" alt="" />
                                                         </span>
-                                                        <span
-                                                            class="flex flex-col"
-                                                        >
+                                                        <span class="flex flex-col">
                                                             <span
-                                                                class="text-left font-bold text-black text-[18px] capitalize"
-                                                            >
+                                                                class="text-left font-bold text-black text-[18px] capitalize">
                                                                 {{
-                                                                    $page.props
-                                                                        .auth
-                                                                        .user
-                                                                        .name
-                                                                }}</span
-                                                            >
+                                        $page.props
+                                            .auth
+                                            .user
+                                            .name
+                                    }}</span>
                                                             <span>
                                                                 {{
-                                                                    $page.props
-                                                                        .auth
-                                                                        .user
-                                                                        .email
-                                                                }}</span
-                                                            >
+                                            $page.props
+                                                .auth
+                                                .user
+                                                .email
+                                        }}</span>
                                                         </span>
 
-                                                        <svg
-                                                            class="ms-2 -me-0.5 h-4 w-4"
-                                                            xmlns="http://www.w3.org/2000/svg"
-                                                            viewBox="0 0 20 20"
-                                                            fill="currentColor"
-                                                        >
-                                                            <path
-                                                                fill-rule="evenodd"
+                                                        <svg class="ms-2 -me-0.5 h-4 w-4"
+                                                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                                            fill="currentColor">
+                                                            <path fill-rule="evenodd"
                                                                 d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                                clip-rule="evenodd"
-                                                            />
+                                                                clip-rule="evenodd" />
                                                         </svg>
                                                     </button>
                                                 </span>
                                             </template>
 
                                             <template #content>
-                                                <DropdownLink
-                                                    :href="
-                                                        route('profile.edit')
-                                                    "
-                                                >
+                                                <DropdownLink :href="route('profile.edit')
+                                        ">
                                                     Profile
                                                 </DropdownLink>
-                                                <DropdownLink
-                                                    :href="route('logout')"
-                                                    method="post"
-                                                    as="button"
-                                                >
+                                                <DropdownLink :href="route('logout')" method="post" as="button">
                                                     Log Out
                                                 </DropdownLink>
                                             </template>
@@ -256,6 +193,14 @@ const showingNavigationDropdown = ref(false);
                     <main class="p-10 text-black h-[90vh]">
                         <div class="bg-white shadow p-10">Content Here</div>
                         <div class="bg-white shadow p-10 mt-10">
+
+                            <slot name="header" />
+
+                            <slot name="content" />
+
+                            <slot />
+
+
                             Content Here
                         </div>
                     </main>
