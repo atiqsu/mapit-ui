1a38f8df4bd8fd0dad2439598071591b1b94036c
Atiq <atiqur.su@gmail.com>
2024-06-25 14:23:39 +0600

    #updated

--- resources/js/Pages/Study/Index.vue
+++ resources/js/Pages/Study/Index.vue
@@ -28,6 +28,14 @@ const form = useForm({
     code: '',
 });
 
+const editStudy = useForm({
+    id: '',
+    name: '',
+    code: '',
+});
+
+
+
 
 const tblCols = ["ID", "name", "Code", ""];
 const modalIsVisible = ref(false);
@@ -43,26 +51,32 @@ const showModalForAddNew = () => {
 const showModalForEdit = (idd) => {
 
     editModalIsVisible.value = true;
+    editStudy.value = idd;
 
-    axios.get(route('studies.edit', idd), { studyId: idd})
-          .then(function (response) {
-        // handle success
-        console.log(response);
-    })
-          .catch(function (error) {
-        // handle error
-        console.log(error);
-    })
-          .finally(function () {
-        // always executed
-    });
+    console.log('wth....', idd, editStudy.value);
+
+
+    // axios.get(route('studies.edit', idd), { studyId: idd})
+    //       .then(function (response) {
+    //
+    //     console.log(response);
+    // })
+    //       .catch(function (error) {
+    //     // handle error
+    //     console.log(error);
+    // })
+    //       .finally(function () {
+    //     // always executed
+    // });
 
     //nextTick(() => nameInputFocus.value.focus());
 };
 
 const closeModal = () => {
     modalIsVisible.value = false;
+    editModalIsVisible.value = false;
     form.reset();
+    editStudy.reset();
 };
 
 const createProject = () => {
@@ -103,7 +117,7 @@ const createProject = () => {
                     <td>{{ study.name }}</td>
                     <td>{{ study.code }}</td>
                     <td>
-                        <ActionBtnEdit @click="showModalForEdit(study.id)">Edit</ActionBtnEdit>
+                        <ActionBtnEdit @click="showModalForEdit(study)">Edit</ActionBtnEdit>
                         <ActionBtnDelete @click="showModalForAddNew">Delete</ActionBtnDelete>
                     </td>
                 </tr>
@@ -167,9 +181,60 @@ const createProject = () => {
         </Modal>
 
 
+
+        <Modal :show="editModalIsVisible" @close="closeModal">
+            <div class="p-6">
+                <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                    Update project info
+                </h2>
+
+                <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                </p>
+
+                <div class="mt-6">
+                    <InputLabel for="name" value="Study name" class="sr-only" />
+
+                    <TextInput
+                        id="name"
+                        ref="nameInputFocus"
+                        v-model="form.name"
+                        class="mt-1 block w-3/4"
+                        placeholder="Study name..."
+                    />
+
+                    <InputError :message="form.errors.name" class="mt-2" />
+                </div>
+
+                <div class="mt-6">
+                    <InputLabel for="code" value="Study code" class="sr-only" />
+
+                    <TextInput
+                        id="code"
+                        v-model="form.code"
+                        class="mt-1 block w-3/4"
+                        placeholder="Study code..."
+                    />
+
+                    <InputError :message="form.errors.code" class="mt-2" />
+                </div>
+
+                <div class="mt-6 flex justify-end">
+                    <SecondaryButton @click="closeModal"> Cancel </SecondaryButton>
+
+                    <SubmitButton
+                        class="ms-3"
+                        :class="{ 'opacity-25': form.processing }"
+                        :disabled="form.processing"
+                        @click="createProject"
+                    >
+                        Create
+                    </SubmitButton>
+                </div>
+            </div>
+        </Modal>
+
+
     </AuthenticatedLayout>
-
-
 
 
 </template>
