a638e9565070317564bc1c96cf9ef71c001a7aaa
Atiq <atiqur.su@gmail.com>
2024-06-26 11:56:20 +0600

    creating new action button

---
+++ resources/js/Components/ActionBtnActive.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+    status: {
+        type: String,
+        default: '1',  // 0 | 1
+    },
+});
+</script>
+
+<template>
+    <button class="">
+
+        <i class="pi pi-pen-to-square text-[#60a5fa] text-[16px]"></i>
+
+        <slot />
+    </button>
+</template>
