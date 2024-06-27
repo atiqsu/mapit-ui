bbc40961c7028ed469820525d0655cf9d0aa8bcb
atiq-xs <game99cu@gmail.com>
2024-06-27 14:07:35 +0600

    #inprogress design

--- resources/js/Pages/Preset/Index.vue
+++ resources/js/Pages/Preset/Index.vue
@@ -2,8 +2,11 @@
 import { nextTick, ref } from "vue";
 import { useForm } from "@inertiajs/vue3";
 import MultiSelect from "primevue/multiselect";
-import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import Select from "primevue/select";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import AddNewButton from "@/Components/AddNewButton.vue";
+import SubmitButton from "@/Components/SubmitButton.vue";
+import CancelButton from "@/Components/CancelButton.vue";
 
 const props = defineProps({
     studies: {
@@ -21,11 +24,6 @@ const props = defineProps({
         },
     },
 });
-
-const selectedPresetTypes = ref([]);
-const selectedPreserveCols = ref([]);
-const selectedFilterOption = ref([]);
-const selectedSheetRelation = ref([]);
 
 const form = useForm({
     name: "",
@@ -53,6 +51,40 @@ const preset = ref({
         sheet6: ["filed", "var", "sdtm"],
     },
 });
+
+const selectedPresetTypes = ref([]);
+const selectedPreserveCols = ref([]);
+const selectedFilterOption = ref([]);
+const selectedSheetRelation = ref(null);
+const selectedSheetCols = ref([]);
+const selectedSheetRelation2 = ref(null);
+const selectedSheetCols2 = ref([]);
+
+// Delete action in preserve cols
+
+const handleDeleteCols = (ind) => {
+    selectedPreserveCols.value.splice(ind, 1);
+};
+
+// Delete action in filter options
+const handleDeleteFilterOption = (ind) => {
+    selectedFilterOption.value.splice(ind, 1);
+};
+
+// Get All Key On Sheets_option
+const sheetOptions = Object.keys(preset.value.sheets_option);
+
+// Change Sheet Relation Data
+const updateSheetRelation = () => {
+    selectedSheetCols.value =
+        preset.value.sheets_option[selectedSheetRelation.value] || [];
+};
+
+// Change Sheet Relation Data Second Part
+const updateSheetRelation2 = () => {
+    selectedSheetCols2.value =
+        preset.value.sheets_option[selectedSheetRelation2.value] || [];
+};
 
 const tblCols = ["ID", "name", "Code", "Actions"];
 const modalIsVisible = ref(false);
@@ -107,19 +139,18 @@ const createProject = () => {
         <template #content>
             <div class="border p-10 rounded-xl">
                 <table class="space-y-7">
-                    <tr class="flex items-center gap-5">
+                    <tr class="flex items-start gap-5">
                         <td class="text-right w-[200px]">
                             <h4 class="text-[24px] text-black">Preset type:</h4>
                         </td>
                         <td class="flex items-center gap-4">
                             <div class="card bg-white flex justify-center">
-                                <MultiSelect
+                                <Select
                                     v-model="selectedPresetTypes"
                                     multiselect.disabled.background
                                     :options="preset.types_option"
                                     filter
                                     placeholder="Select Preset Types"
-                                    :maxSelectedLabels="3"
                                     class="w-full md:w-52 bg-white"
                                 />
                             </div>
@@ -130,17 +161,16 @@ const createProject = () => {
                             />
                         </td>
                     </tr>
-                    <tr class="flex items-center gap-5">
+                    <tr class="flex items-start gap-5">
                         <td class="text-right w-[200px]">
                             <h4 class="text-[24px] text-black">
                                 Preserve col:
                             </h4>
                         </td>
                         <td>
-                            <div class="card bg-white flex justify-center">
+                            <div class="card bg-white flex">
                                 <MultiSelect
                                     v-model="selectedPreserveCols"
-                                  
                                     :options="preset.cols_option"
                                     filter
                                     placeholder="Select Preserve col"
@@ -148,45 +178,119 @@ const createProject = () => {
                                     class="w-full md:w-52 bg-white"
                                 />
                             </div>
+                            <ul class="flex items-center gap-4 mt-6">
+                                <li
+                                    v-for="(
+                                        selectedPreserveCol, index
+                                    ) in selectedPreserveCols"
+                                    :key="index"
+                                    class="w-full md:w-52 rounded py-2 px-3 border border-[#e5e7eb] hover:border-[#94a3b8] flex items-center justify-between"
+                                >
+                                    {{ selectedPreserveCol }}
+                                    <CancelButton
+                                        @click="handleDeleteCols(index)"
+                                        class="px-1 py-1 dark:bg-[#e5e7eb] border-0 dark:text-slate-400 bg-transparent dark:hover:bg-[#e5e7eb] rounded-full w-3 h-3 flex items-center justify-center hover:text-slate-600 focus:ring-0 focus:ring-offset-0"
+                                    >
+                                        <i
+                                            class="pi pi-times-circle text-[0.5rem]"
+                                        ></i>
+                                    </CancelButton>
+                                </li>
+                            </ul>
                         </td>
                     </tr>
-                    <tr class="flex items-center gap-5">
+                    <tr class="flex items-start gap-5">
                         <td class="text-right w-[200px]">
                             <h4 class="text-[24px] text-black">Filter:</h4>
                         </td>
                         <td>
-                            <div class="card bg-white flex justify-center">
+                            <div class="card bg-white flex">
                                 <MultiSelect
                                     v-model="selectedFilterOption"
-                                
                                     :options="preset.filter_option"
-                                
                                     filter
                                     placeholder="Select Filter Option"
                                     :maxSelectedLabels="3"
                                     class="w-full md:w-52 bg-white"
                                 />
                             </div>
+                            <ul class="flex items-center gap-4 mt-6">
+                                <li
+                                    v-for="(
+                                        selectedFilters, index
+                                    ) in selectedFilterOption"
+                                    :key="index"
+                                    class="w-full md:w-52 rounded py-2 px-3 border border-[#e5e7eb] hover:border-[#94a3b8] flex items-center justify-between"
+                                >
+                                    {{ selectedFilters }}
+                                    <CancelButton
+                                        @click="handleDeleteFilterOption(index)"
+                                        class="px-1 py-1 dark:bg-[#e5e7eb] border-0 dark:text-slate-400 bg-transparent dark:hover:bg-[#e5e7eb] rounded-full w-3 h-3 flex items-center justify-center hover:text-slate-600 focus:ring-0 focus:ring-offset-0"
+                                    >
+                                        <i
+                                            class="pi pi-times-circle text-[0.5rem]"
+                                        ></i>
+                                    </CancelButton>
+                                </li>
+                            </ul>
                         </td>
                     </tr>
-                    <tr class="flex items-center gap-5">
+                    <tr class="flex items-start gap-5">
                         <td class="text-right w-[200px]">
                             <h4 class="text-[24px] text-black">
                                 Sheet Relation:
                             </h4>
                         </td>
-                        <td>
-                            <div class="card bg-white flex justify-center">
-                                <MultiSelect
-                                    v-model="selectedSheetRelation"
-                                 
-                                    :options="preset.sheets_option.key"
-                                 
-                                    filter
-                                    placeholder="Select Filter Option"
-                                    :maxSelectedLabels="3"
-                                    class="w-full md:w-52 bg-white"
-                                />
+                        <td class="space-y-4">
+                            <div class="flex items-center gap-4">
+                                <div class="card bg-white flex justify-center">
+                                    <Select
+                                        v-model="selectedSheetRelation"
+                                        :options="sheetOptions"
+                                        filter
+                                        placeholder="Sheet"
+                                        class="w-full md:w-52 bg-white"
+                                        @change="updateSheetRelation"
+                                    />
+                                </div>
+                                <div class="card bg-white flex justify-center">
+                                    <Select
+                                        v-model="selectedSheetCols"
+                                        :options="selectedSheetCols"
+                                        filter
+                                        placeholder="Columns"
+                                        class="w-full md:w-52 bg-white"
+                                    />
+                                </div>
+                                <p>
+                                    <i
+                                        class="pi pi-arrow-right text-slate-500"
+                                    ></i>
+                                </p>
+                                <div class="card bg-white flex justify-center">
+                                    <Select
+                                        v-model="selectedSheetRelation2"
+                                        :options="sheetOptions"
+                                        filter
+                                        placeholder="Sheet"
+                                        class="w-full md:w-52 bg-white"
+                                        @change="updateSheetRelation2"
+                                    />
+                                </div>
+                                <div class="card bg-white flex justify-center">
+                                    <Select
+                                        v-model="selectedSheetCols2"
+                                        :options="selectedSheetCols2"
+                                        filter
+                                        placeholder="Columns"
+                                        class="w-full md:w-52 bg-white"
+                                    />
+                                </div>
+                                <AddNewButton />
+                            </div>
+                            <div class="flex items-center gap-4">
+                                <SubmitButton class="ms-0"> Save </SubmitButton>
+                                <CancelButton> Close </CancelButton>
                             </div>
                         </td>
                     </tr>
